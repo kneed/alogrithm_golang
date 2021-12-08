8599d5ee07a1408330fc155b3432785e63102809
kealsg <xie_ke_ok@126.com>
2021-12-08 23:57:31 +0800

    feat: 格子里的路径(动态规范解法)
    暴力计算所有可能, 通过dp数组来减少计算步骤.

---
+++ 62_medium_不同路径.go
@@ -0,0 +1,29 @@
+package main
+
+//一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+//
+//机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+//
+//问总共有多少条不同的路径？
+
+func uniquePaths(m int, n int) int {
+	var dp = make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 {
+				dp[i][j] = 1
+				continue
+			}
+			if j == 0 {
+				dp[i][j] = 1
+				continue
+			}
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
